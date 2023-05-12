6a7def4849244f038eea4032633fa62810184bcd
Josue Hernandez Ramirez <ingscjoshua@gmail.com>
2023-05-11 18:47:02 -0600

    base project

---
+++ src/demo_payment_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { demo_payment_backend } from "../../declarations/demo_payment_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await demo_payment_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
